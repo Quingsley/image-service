781a5e0f99a073d2c7afaba8f049b6ccb4576df0
quingsley <quingsley@hotmail.com>
2023-04-08 23:29:00 +0300

    fixing delete endpoint

--- controllers/delete.js
+++ controllers/delete.js
@@ -5,8 +5,7 @@ const deleteImageController = async (req, res, next) => {
     const { data, error } = await supabase.storage
       .from(`${process.env.BUCKET}`)
       .remove([`public/${fileName}`]);
-
-    if (error || error === null) {
+    if (error && error === null) {
       res.status(500).json({
         message: "Unable to delete Image",
       });
